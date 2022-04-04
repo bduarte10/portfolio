f0395146b9afdfd029bff2993c429e581cb313d2
Bruno Duarte <duartebruno2208@gmail.com>
2022-04-04 14:17:05 -0300

    add mobile menu

---
+++ script.js
@@ -0,0 +1,13 @@
+let menu = document.getElementById('menu-hamburguer');
+let toggle = document.querySelector('.menu-toggle');
+console.log(menu);
+
+menu.addEventListener('click', () => {
+	menu.classList.toggle('fa-xmark');
+	toggle.classList.toggle('active');
+});
+
+window.onscroll = () => {
+	menu.classList.remove('fa-xmark');
+	toggle.classList.remove('active');
+};
