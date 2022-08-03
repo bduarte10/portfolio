9d33e7fad99c7e31a8c4ecace763590d315a5dfc
Tiago França <dev@tiagofranca.com>
2022-08-03 16:48:27 -0300

    front inprovements
    Mudanças a serem submetidas:
    	modified:   index.html
    	modified:   script.js
    	modified:   styles.css

--- script.js
+++ script.js
@@ -7,7 +7,38 @@ menu.addEventListener('click', () => {
 	toggle.classList.toggle('active');
 });
 
+function reachedTheLimit(minHeight = null) {
+	minHeight = !minHeight || (typeof minHeight != "number") ? 150 : minHeight;
+
+	// window.pageYOffset || document.documentElement.scrollTop // Alternativa?
+	if (document.body.scrollTop >= minHeight || document.documentElement.scrollTop >= minHeight) {
+		return true
+	}
+
+	return false
+}
+
+function srinkMenu(toTop = true) {
+	toTop = (typeof toTop != "boolean") ? true : toTop;
+
+	let menuContainer = document.querySelector('header > div.container');
+
+	if (!menuContainer) {
+		return
+	}
+
+	if (toTop) {
+		menuContainer.classList.remove('p-0')
+		return
+	}
+
+	menuContainer.classList.add('p-0')
+}
+
 window.onscroll = () => {
 	menu.classList.remove('fa-xmark');
 	toggle.classList.remove('active');
+
+	let screenBased = (window.screen.height / 100) * 30;
+	srinkMenu(reachedTheLimit(screenBased))
 };
